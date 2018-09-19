341ea302740e4908e823f0dae9186510d785262c
347862 <dileep.hareendran@cognizant.com>
2018-09-20 04:24:34 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from '../../node_modules/rxjs';
+import { DataModel } from './data/data.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'angu-d3js';
+
+  data: Observable<DataModel>;
+
+  constructor(private http: HttpClient) {
+    this.data = this.http.get<DataModel>('assets/data/data.json');
+  }
 }
